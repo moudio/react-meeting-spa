4c6f81f8d4e5897f4797d9335dbee813e39531a9
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2020-07-13 10:09:08 +0000

    Handle login in

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,56 @@
 import React, { Component } from 'react';
+import firebase from './Firebase';
+import FormError from './FormError';
+import { navigate } from '@reach/router';
 
 export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      errorMessage: null,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    const itemName = e.target.name;
+    const itemValue = e.target.value;
+    this.setState({ [itemName]: itemValue });
+  }
+  handleSubmit(e) {
+    let registrationInfo = {
+      email: this.state.email,
+      password: this.state.password,
+    };
+
+    e.preventDefault();
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(
+        registrationInfo.email,
+        registrationInfo.password
+      )
+      .then(() => {
+        navigate('/meetings');
+      })
+      .catch((error) => {
+        if (error.message !== null) {
+          this.setState({
+            errorMessage: error.message,
+          });
+        } else {
+          this.setState({
+            errorMessage: null,
+          });
+        }
+      });
+  }
   render() {
     return (
-      <form className="mt-3">
+      <form className="mt-3" onSubmit={this.handleSubmit}>
         <div className="container">
           <div className="row justify-content-center">
             <div className="col-lg-6">
@@ -11,6 +58,9 @@ export default class Login extends Component {
                 <div className="card-body">
                   <h3 className="font-weight-light mb-3">Log in</h3>
                   <section className="form-group">
+                    {this.state.errorMessage !== null ? (
+                      <FormError theMessage={this.state.errorMessage} />
+                    ) : null}
                     <label
                       className="form-control-label sr-only"
                       htmlFor="Email"
@@ -24,6 +74,8 @@ export default class Login extends Component {
                       id="email"
                       name="email"
                       placeholder="Email"
+                      value={this.state.email}
+                      onChange={this.handleChange}
                     />
                   </section>
                   <section className="form-group">
@@ -33,6 +85,8 @@ export default class Login extends Component {
                       type="password"
                       name="password"
                       placeholder="Password"
+                      value={this.state.password}
+                      onChange={this.handleChange}
                     />
                   </section>
                   <div className="form-group text-right mb-0">
